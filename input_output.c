c4b058f6509356d5bc66c76299057c150b2099af
Dave Tang <davetang@davetang-VirtualBox.(none)>
2014-04-25 15:13:48 +0900

    Added comments

--- input_output.c
+++ input_output.c
@@ -4,8 +4,27 @@ int main()
 {
    int this_is_a_number;
    printf("Please enter a number: ");
+
+   /*
+   http://www.cplusplus.com/reference/cstdio/scanf/
+
+   Read formatted data from stdin
+
+   A format specifier for scanf follows this prototype:
+
+   %[*][width][length]specifier 
+
+   To store the result of a scanf operation on a regular variable, its name should be preceded by the reference operator (&)
+
+   */
    scanf("%d", &this_is_a_number);
+
    printf("You entered %d\n", this_is_a_number);
+
+   /*
+   Get character from stdin
+   */
    getchar();
+
    return 0;
 }
