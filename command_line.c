2ac95b411cd7b3158900ae351b2162c180092c57
Dave Tang <davetang@davetang-VirtualBox.(none)>
2014-04-25 15:34:39 +0900

    C for command line argument

---
+++ command_line.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*
+http://www.tutorialspoint.com/cprogramming/c_command_line_arguments.htm
+Command line arguments are handled using the main() function arguments,
+where argc refers to the number of arguments passed,
+and argv[] is a pointer array which points to each argument passed to the program.
+*/
+
+int main( int argc, char *argv[] )  
+{
+   char my_file_name;
+   printf("The number of arguments passed is %d\n", argc);
+
+   /*
+   argv[0] is this file
+   */
+
+   int i;
+   for(i = 0; i < argc; ++i){
+      printf("argv[%d] is %s\n", i, argv[i]);
+   }
+
+   return(0);
+}
