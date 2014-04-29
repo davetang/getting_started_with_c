ff421ca781907634855e1ace306efec85c71f621
Dave Tang <tan118@tan118.(none)>
2014-04-30 00:11:27 +0900

    C data types on Wikipedia

--- data_type.c
+++ data_type.c
@@ -2,6 +2,22 @@
 
 int main(void)
 {
+
+   /*
+   http://en.wikipedia.org/wiki/C_data_types
+
+   In the C programming language, data types refers to an extensive system for declaring variables of different types.
+
+   The language itself provides basic arithmetic types and syntax to build array and compound types.
+
+   Several headers in the standard library contain definitions of support types, that have additional properties, such as exact size, guaranteed.
+
+   The C language provides many basic types.
+
+   Most of them are formed from one of the four basic arithmetic type specifiers in C (char, int, float and double), and optional specifiers (signed, unsigned, short, long).
+
+   */
+
    /* http://www.learnconline.com/2010/04/data-types-in-c-programming-language.html */
 
    /* int is used to define integer numbers. It occupies 2 bytes of memory (16-bit compiler) */
@@ -36,6 +52,7 @@ int main(void)
    long double my_long_double;
    my_long_double = 1.2000000000001;
    printf("my_long_double -> %Lf\n", my_long_double);
+
 
    return 0;
 }
