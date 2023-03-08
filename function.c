b35ae1af07863a16f46b5a61f1e9e3e9322e9dc7
Dave Tang <davetingpongtang@gmail.com>
2023-03-08 15:14:48 +0900

    Newton-Raphson

--- function.c
+++ function.c
@@ -4,45 +4,79 @@
 /*
    In C, functions must be first defined before they are used
 
-   They can be either declared first and then implemented later on using a header file or in the beginning of the C file
+   They can be either declared first and then implemented later on using a
+   header file or in the beginning of the C file
 
    C functions receive either a fixed or variable amount of arguments
 
    Functions can only return one value, or none
+
+   The first line of a function definition tells the compiler four things about
+   the function (from left to right):
+
+   1. Who can call it
+   2. The type of value it returns
+   3. Its name
+   4. The arguments it takes
 */
 
-int multiply_by_ten(int x){
+int times_ten(int x){
    return x * 10;
 }
 
 /*
-
    Functions with variable argument lists in C using va_list
 
    http://www.cprogramming.com/tutorial/c/lesson17.html
-
 */
 
-int multiply (int num, ... ){   
-   int total = 0;
-   va_list arguments;
-   int x;
+int product (int size, ... ){   
+   int total = 1;
+   va_list numbers;
 
-   va_start (arguments, num);
+   va_start (numbers, size);
 
-   for (x = 0; x < num; x++){   
-      total *= va_arg(arguments, int);
+   for ( int x = 0; x < size; x++ ){
+      total *= va_arg(numbers, int);
    }
-   va_end (arguments);
+   va_end (numbers);
 
    return total;
 }
 
-int main (){
-   printf("%d\n", multiply_by_ten(10));
+/*
+   Newton-Raphson method for computing the square root:
 
-   printf("%d\n", multiply(3, 10, 5, 2));
+   1. Set guess to 1
+   2. If |guess^2 - x| < limit, proceed to step 4
+   3. Set guess to (x / guess + guess) / 2 and return to step 2
+   4. Return guess, which is the approximation of the square root
+*/
+
+float abs_ (float x){
+   if (x < 0)
+      x = -x;
+   return x;
+}
+
+double sqrt_ (double x){
+   // const to declare that a variable's value will not be changed
+   const double e = 0.000001;
+   double guess = 1.0;
+   while ( abs_(guess * guess - x) >= e ){
+      printf("Guessing %f\n", guess);
+      guess = ( x / guess + guess ) / 2.0;
+   }
+   return guess;
+}
+
+int main (void){
+   printf("10 times 10 is %d\n", times_ten(10));
+
+   printf("The product of 1, 2, 3, 4, and 5 is %d\n", product(5, 1, 2, 3, 4, 5));
+
+   printf("The square root of 25 is %f\n", sqrt_(25));
+   printf("The square root of 28 is %f\n", sqrt_(28));
   
    return 0;
 }
-
